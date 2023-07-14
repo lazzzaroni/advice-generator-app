8c6316c1427d4a2c9ce4a3594eafd42ec71cfdce
lazzzaroni <iamnotlazzaroni@gmail.com>
2023-07-14 18:50:29 +0200

    done

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,8 +1,45 @@
 import Image from "next/image";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 import { cn } from "@/lib/utils";
 
+type ApiData = {
+  slip: {
+    id: number;
+    advice: string;
+  };
+};
+
 export default function Container() {
+  const { data, isLoading, refetch, isError } = useQuery<ApiData>({
+    queryKey: ["advice"],
+    queryFn: async () => {
+      const response = await axios.get("https://api.adviceslip.com/advice");
+      return response.data as ApiData;
+    },
+  });
+
+  if (isError) {
+    return (
+      <div
+        className={cn(
+          "relative flex h-auto w-[343px] flex-col items-center justify-center rounded-xl bg-neutral-dark-grayish-blue px-6 pb-12 pt-10 shadow-lg",
+          "md:w-[540px] md:p-12"
+        )}
+      >
+        <p
+          className={cn(
+            " text-center text-2xl font-bold leading-snug",
+            "md:w-4/5 md:text-[28px] md:leading-tight"
+          )}
+        >
+          Something went wrong. Please try again later!
+        </p>
+      </div>
+    );
+  }
+
   return (
     <section
       className={cn(
@@ -15,13 +52,17 @@ export default function Container() {
           "fit mb-6 text-xs font-extrabold uppercase tracking-[0.3em] text-primary-neon-green",
           "md:text-sm"
         )}
-      >{`Advice #${117}`}</h1>
+      >
+        Advice #<span>{isLoading ? "..." : data?.slip.id}</span>
+      </h1>
       <p
         className={cn(
           "text-center text-2xl font-bold leading-snug",
           "md:text-[28px] md:leading-tight"
         )}
-      >{`"It is easy to sit up and take notice, what's difficult is getting up and taking action."`}</p>
+      >
+        {isLoading ? "Loading..." : <span>{data?.slip.advice}</span>}
+      </p>
       <picture className={cn("mb-4 mt-6", "md:mb-6 md:mt-10")}>
         <source
           media="(min-width:768px)"
@@ -37,6 +78,7 @@ export default function Container() {
         className={cn(
           "absolute bottom-0 left-1/2 h-16 w-16 -translate-x-1/2 translate-y-1/2 rounded-full bg-primary-neon-green transition duration-200 ease-in-out hover:shadow-[0_0_32px_0] hover:shadow-primary-neon-green active:scale-95"
         )}
+        onClick={() => refetch()}
       >
         <Image
           src="/icon-dice.svg"
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,19 +2,27 @@ import { type AppType } from "next/dist/shared/lib/utils";
 
 import "@/styles/globals.css";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 import { fontSans } from "@/lib/fonts";
 import { cn } from "@/lib/utils";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
+  const queryClient = new QueryClient();
+
   return (
-    <main
-      className={cn(
-        "flex min-h-screen flex-col items-center justify-center bg-neutral-dark-blue font-sans text-primary-light-cyan",
-        fontSans.variable
-      )}
-    >
-      <Component {...pageProps} />
-    </main>
+    <QueryClientProvider client={queryClient}>
+      <main
+        className={cn(
+          "flex min-h-screen flex-col items-center justify-center bg-neutral-dark-blue font-sans text-primary-light-cyan",
+          fontSans.variable
+        )}
+      >
+        <Component {...pageProps} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </main>
+    </QueryClientProvider>
   );
 };
 
